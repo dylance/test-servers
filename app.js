2f7bf64a4ed45504e2fc7b8ef097eabc58a93d1c
Dylan Ellison <dylancellison@gmail.com>
2018-11-25 00:56:44 -0600

    moved generateRandomString into utilities folder

--- app.js
+++ app.js
@@ -6,34 +6,19 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
-
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
+const generateRandomString = require("./utility/generateRandomString")
 
 var client_id = 'a829d5c8ef3944eba13ddea08af2dd7d'; // Your client id
 var client_secret = '695d8bfef5fb46dd9ce8a9e9420f1159'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
+var app = express();
 
 var stateKey = 'spotify_auth_state';
-
-var app = express();
 
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
