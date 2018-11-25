f51e98ad7d9c76d309849a9a7e6b2c79b09abbb0
Dylan Ellison <dylancellison@gmail.com>
2018-11-25 01:18:32 -0600

    changed class to className in jsx

--- client/src/App.js
+++ client/src/App.js
@@ -10,19 +10,20 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav class="navbar navbar-expand-lg navbar-light " >
+        <nav className="navbar navbar-expand-lg navbar-light " >
           {/* <a class="navbar-brand" href="">Mixify</a> */}
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse nav-left-margin" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              {/* <li class="nav-item active search-button">
-                <a class="nav-link navtext" href="">Search<span class="sr-only">(current)</span></a>
+          <div className="collapse navbar-collapse nav-left-margin" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              {/* <li className="nav-item active search-button">
+                <a className="nav-link navtext" href="">Search<span className="sr-only">(current)</span></a>
               </li> */}
             </ul>
-            <a class="text-black" href='http://localhost:8888'> Login to Spotify </a>
+            <a className="text-black" href='http://localhost:8888'> Login to Spotify </a>
+            <a className="text-black" href='/login'> Login to Spotify on the server </a>
           </div>
         </nav>
         <h1> Mixify </h1>
